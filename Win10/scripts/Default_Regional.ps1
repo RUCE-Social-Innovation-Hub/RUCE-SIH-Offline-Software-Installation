9c7253da5b63254755fab17193d3f13631b938f9
DST-MAC-15 <dst-mac-15@dhcp-602a.wifin.ru.ac.za>
2022-07-30 13:19:45 +0200

    split the script into 3 categories: SIH devices full installation, Other people installation who will not need all the software we use for training and who will not be given a reegistered copy of MS Word

--- Win10/scripts/Default_Regional.ps1
+++ Win10/scripts/Default_Regional.ps1
@@ -6,6 +6,8 @@ Set-WinSystemLocale en-ZA
 Set-WinHomeLocation -GeoId 209
 Set-WinUserLanguageList en-ZA
 reg load $DefaultHKEY $DefaultRegPath
-reg import "\\rhino\sw01\Software\OEM\Win10\Laptop\en-za-default.reg"
+# reg import "\\rhino\sw01\Software\OEM\Win10\Laptop\en-za-default.reg"
+reg import ".\en-za-default.reg"
 reg unload $DefaultHKEY
-reg import "\\rhino\sw01\Software\OEM\Win10\Laptop\en-za-welcome.reg"
+# reg import "\\rhino\sw01\Software\OEM\Win10\Laptop\en-za-welcome.reg"
+reg import ".\en-za-welcome.reg"
